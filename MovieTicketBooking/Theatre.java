3d3f6c3095dbf147c75f81610dbd0991a476d506
Nandhagopalan107 <85563826+Nandhagopalan107@users.noreply.github.com>
2023-07-03 00:16:53 +0530

    Update Theatre.java

--- MovieTicketBooking/Theatre.java
+++ MovieTicketBooking/Theatre.java
@@ -1,4 +1,6 @@
 import java.util.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 class Theatre 
 {
@@ -7,28 +9,28 @@ class Theatre
 	String location;
 	String final_seats;
 	boolean ac;
-	Movie movie;
-	int capacity=100;
-	public static String[][] seats = new String[11][11];
+	String movie;
+	int capacity=100,timingl = 4;
+	String[][] seats = new String[11][11];
 	
 	Theatre(String name)
 	{
 		
 		this.theatreName=name;
-		for(String[] i:seats)
+		for(String[] i:this.seats)
 		{
 			Arrays.fill(i,"*");	
 		}
 		char v='a';int v2=1;
 		for(char j = 1;j<11;j++)
 		{
-			seats[j][0]=Character.toString(v);
-			seats[0][j]=Integer.toString(v2);
+			this.seats[j][0]=Character.toString(v);
+			this.seats[0][j]=Integer.toString(v2);
 			v2++;
 			v++;
 		}
 	}
-	public  static void viewSeats()
+	public  static void viewSeats(String[][] seats)
 	{	
 		System.out.println("\n\n---Seating arrangements---");
 		for(String[] i:seats)
@@ -40,11 +42,11 @@ class Theatre
 				System.out.println();
 			}
 	}
-	public String selectTickets()
-	{		
-			System.out.println("\nYou selected "+theatreName);
-			
-			viewSeats();
+	public String selectTickets(String movieName,String time)
+	{		this.movie = movieName;
+
+
+			viewSeats(this.seats);
 			System.out.print("\nEnter the no of tickets: ");
 			
 			int tickets = sc.nextInt();
@@ -59,9 +61,9 @@ class Theatre
 					char r = s.charAt(0);int col=Character.getNumericValue(s.charAt(1)),row=(int)r-96;
 					
 					
-					if(seats[row][col]=="*")
+					if(this.seats[row][col]=="*")
 					{
-						seats[row][col] = "x";
+						this.seats[row][col] = "x";
 						final_seats+=s+" ";
 						
 					}
@@ -75,10 +77,16 @@ class Theatre
 			else{
 				System.out.println("Insufficient tickets. Available no of tickets: "+ capacity);
 			}
+			this.printTicketdetails(time);
 			return final_seats;
 
 
 
+	 }
+	 public   void printTicketdetails(String time)
+	 {
+	 	System.out.println("\nYour Ticket details:");
+	 	System.out.println("Movie name: "+movie+"\nTheatre Name :"+theatreName+"\n"+"Shsowtime: "+time+"\nSeat no :"+final_seats);
 	 }
 	 public void details()
 	 {
