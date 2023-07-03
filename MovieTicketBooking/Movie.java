9ff7f4077627879f8697972cf32aa2b55a7c890d
Nandhagopalan107 <85563826+Nandhagopalan107@users.noreply.github.com>
2023-07-03 09:37:07 +0530

    Update Movie.java

--- MovieTicketBooking/Movie.java
+++ MovieTicketBooking/Movie.java
@@ -1,62 +1,103 @@
 import java.util.*;
-class Movie 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+class Theatre 
 {
+	Scanner sc  = 	new Scanner(System.in);
+	String theatreName;
+	String location;
+	String final_seats;
+	boolean ac;
+	String movie;
+	int capacity=100,timingl = 4;
+	String[][] seats = new String[11][11];
 	
-	Scanner sc = new Scanner(System.in);
-	String[] names = new String[]{"ROLEX","GPN","PVR"};
-	
-	int timingl = 4,showtime;
-	List<Theatre> theatre = new ArrayList() ;
-	String[] Btimings = new String[]{"10.00 am","2.00 pm","6.30 pm","10.00pm"};
-	String[] Stimings = new String[4];
-	public String movieName;
-	Movie(String name,int x)
+	Theatre(String name)
 	{
-		this.movieName = name;
-		for(int i=0;i<x;i++)
+		
+		this.theatreName=name;
+		for(String[] i:this.seats)
 		{
-		this.theatre.add(new Theatre(names[i]));
-		for(int j=0;j<x && j<this.timingl;j++)
-		{
-				this.Stimings[j]=this.Btimings[j];
-
-				
+			Arrays.fill(i,"*");	
 		}
-		
-		for(int k=0,j=x+1;j<this.timingl && k<timingl-x;j++,k++)
+		char v='a';int v2=1;
+		for(char j = 1;j<11;j++)
 		{
-			this.Btimings[k]=this.Btimings[j];
-		}
-		
-		
+			this.seats[j][0]=Character.toString(v);
+			this.seats[0][j]=Integer.toString(v2);
+			v2++;
+			v++;
 		}
 	}
-
-	
-	public List<String> BookTicket()
+	public  static void viewSeats(String[][] seats)
 	{	
-		List<String> ticketDetails = new ArrayList<>();
-		System.out.println("\nYou selected "+movieName+"\nSelect the theatre no:");
-		for(int i=1;i<=theatre.size();i++)
+		System.out.println("\n\n---Seating arrangements---");
+		for(String[] i:seats)
 			{
-				System.out.println(i+"."+(theatre.get(i-1).theatreName));
+				for(String j : i)
+				{
+					System.out.print(j+" ");
+				}
+				System.out.println();
 			}
-		int tno =sc.nextInt();
-		System.out.println("\nYou selected "+theatre.get(tno-1).theatreName);
-		
-		System.out.println("\nselect ShowTimeno");
-			for(int i=0;i<this.Stimings.length;i++)
-			{
-					System.out.println(i+1+"-"+this.Stimings[i]);
-			}
-
-			 this.showtime = sc.nextInt();
-
-		String seatno = theatre.get(tno-1).selectTickets(movieName,Stimings[showtime-1]);
-		ticketDetails.add(theatre.get(tno-1).theatreName);
-		ticketDetails.add(seatno);
-		ticketDetails.add(Stimings[showtime-1]);
-		return ticketDetails;
 	}
-	
+	public String selectTickets(String movieName,String time)
+	{		this.movie = movieName;
+
+
+			viewSeats(this.seats);
+			System.out.print("\nEnter the no of tickets: ");
+			
+			int tickets = sc.nextInt();
+			final_seats="";
+			if(capacity>=tickets)
+			{
+				capacity-=tickets;
+				for(int i=1;i<=tickets;i++)
+				{
+					System.out.println("Enter the seat no like a1 b2 c3: ");
+					String s=sc.next();
+					char r = s.charAt(0);int col=Character.getNumericValue(s.charAt(1)),row=(int)r-96;
+					
+					
+					if(this.seats[row][col]=="*")
+					{
+						this.seats[row][col] = "x";
+						final_seats+=s+" ";
+						
+					}
+					else{
+						System.out.println("Sorry! This seat was already booked.");
+						i--;
+					}
+
+				}
+				System.out.println("Total cost of the tickets:"+(tickets*100));
+			}
+			else{
+				System.out.println("Insufficient tickets. Available no of tickets: "+ capacity);
+			}
+
+			this.printTicketdetails(time);
+			return final_seats;
+
+
+
+	 }
+	 public   void printTicketdetails(String time)
+	 {
+	 	System.out.println("\nSucessfully Booked tickets.\n\nYour Ticket details:");
+	 	System.out.println("Movie name: "+movie+"\nTheatre Name :"+theatreName+"\n"+"Showtime: "+time+"\nSeat no :"+final_seats);
+	 }
+	 public void details()
+	 {
+		System.out.println("Theatre Name: "+this.theatreName);
+		System.out.println("Theatre Location: "+this.location);
+		System.out.println("Ac : "+this.ac);
+	//	System.out.println("No of screens : "+this.no_of_screens);
+
+	}
+
+
 }
